efc59f8ee4da4ba5c0198f542fd9b20cd0a1ffb8
Lau Yung <lauy1997@gmail.com>
2018-01-05 10:48:54 +0800

    Add file extension to PDFs to be sent to dept

--- send_mail.php
+++ send_mail.php
@@ -118,7 +118,7 @@
       $result = mysqli_store_result($db);
       $row = mysqli_fetch_row($result);
       $files[$files_count]['path'] = $row[0];
-      $files[$files_count]['name'] = $row[1];
+      $files[$files_count]['name'] = $row[1] . ".pdf";
       $files_count++;
   }
 
@@ -128,7 +128,7 @@
     if($result = mysqli_store_result($db)) {
       while($row = mysqli_fetch_row($result)) {
         $files[$files_count]['path'] = $row[0];
-        $files[$files_count]['name'] = $row[1];
+        $files[$files_count]['name'] = $row[1] . ".pdf";
         $files_count++;
       }
     }
